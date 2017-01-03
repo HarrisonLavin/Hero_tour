231a038cdf5233451e41bf3f8eb4834af0964f22
Lavin <harrison.lavin@gmail.com>
2017-01-03 12:33:16 -0500

    finished!

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,9 +8,11 @@ import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
+import {HeroSearchComponent}    from './hero-search.component';
 
 import { AppRoutingModule }     from './app-routing.module';
 
+import './rxjs-extensions';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -28,7 +30,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
